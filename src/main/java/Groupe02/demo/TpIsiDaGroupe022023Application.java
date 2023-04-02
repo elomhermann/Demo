381c55b127837125d64dc2b46c39cb63377f7ed2
CodeurTom <agnekitomm@gmail.com>
2023-04-02 03:39:15 +0200

    First commit

---
+++ src/main/java/Groupe02/demo/TpIsiDaGroupe022023Application.java
@@ -0,0 +1,13 @@
+package Groupe02.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIsiDaGroupe022023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIsiDaGroupe022023Application.class, args);
+	}
+
+}
---
+++ src/test/java/Groupe02/demo/TpIsiDaGroupe022023ApplicationTests.java
@@ -0,0 +1,13 @@
+package Groupe02.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIsiDaGroupe022023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
